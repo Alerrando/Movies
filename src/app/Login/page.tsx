9bdaae2058b72aba3995927d9a5cad747bd72bc2
Alerrando <alerrando2@gmail.com>
2023-03-12 21:16:21 -0300

    add form and img form

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
 export default function LoginPage(){
     return(
         <>
-            <h1>Hello World</h1>
+            <main className={styles.main}>
+                <div className={styles.mainImg}>
+                    <Image src='/background-login.jpg' alt='image-background-login' fill quality={100} />
+                </div>
+
+                <section className={styles.mainContainer}>
+                    <div className={styles['main-container-img']}>
+                        <Image src='/login-img.jpg' alt='image-background-form-login' fill className={styles['img-form-login']} />
+                    </div>
+
+                    <form className={styles.form}>
+                        
+                    </form>
+                </section>
+            </main>
         </>
     )
 }

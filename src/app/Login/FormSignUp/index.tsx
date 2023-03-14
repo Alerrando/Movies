61c6b9a665f104a83ca6fc2ba4accb40ec61d4bc
Alerrando <alerrando2@gmail.com>
2023-03-14 01:03:03 -0300

    structure FormSignUp

---
+++ src/app/Login/FormSignUp/index.tsx
@@ -0,0 +1,63 @@
+import { AiOutlineMail } from 'react-icons/ai';
+import { BiUser } from 'react-icons/bi';
+import { MdPassword } from 'react-icons/md';
+import { LoginFormInputs } from '../../Login/FormLogin';
+import styles from '../styles.module.scss';
+import { useForm } from 'react-hook-form';
+
+type RegisterFormInputs = {
+  name: string;
+} & LoginFormInputs;
+
+export default function FormSignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<RegisterFormInputs>();
+
+  return (
+    <form className={styles.formInputContainer} onSubmit={handleSubmit(submit)}>
+      <div className={styles['form-input']}>
+        <BiUser size={28} />
+        <input
+          type="text"
+          placeholder="Type your name"
+          {...register('name', { required: true })}
+        />
+      </div>
+
+      <div className={styles['form-input']}>
+        <AiOutlineMail size={28} />
+        <input
+          type="email"
+          placeholder="Type your email"
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+      </div>
+
+      <div className={styles['form-input']}>
+        <MdPassword size={28} />
+        <input
+          type="password"
+          placeholder="Type your password"
+          {...register('password', { required: true, minLength: 5 })}
+        />
+      </div>
+
+      <button
+        className={`${styles.btn} ${
+          isValid ? styles['valid'] : styles['notValid']
+        }`}
+        disabled={!isValid}
+        type="submit"
+      >
+        Create Account
+      </button>
+    </form>
+  );
+
+  function submit(event: RegisterFormInputs) {
+    console.log(event);
+  }
+}

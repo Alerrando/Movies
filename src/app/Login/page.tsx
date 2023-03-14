334c35ced5eb3e0271bd16f4a7cc44a49033f340
Alerrando <alerrando2@gmail.com>
2023-03-14 01:11:49 -0300

    call folders Form

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,28 +1,99 @@
-import Image from 'next/image'
-import styles from './styles.module.scss'
+'use client'
 
-export default function LoginPage(){
-    return(
-        <>
-            <main className={styles.main}>
-                <div className={styles.mainImg}>
-                    <Image src='/background-login.jpg' alt='image-background-login' fill quality={100} />
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { BsFacebook, BsTwitter } from 'react-icons/bs';
+import FormLogin from '../Login/FormLogin';
+import FormSignUp from '../Login/FormSignUp';
+import styles from './styles.module.scss';
+
+type LoginSignUp = {
+  login: 'Login' | 'SignUp';
+};
+
+const inicialValue: LoginSignUp = {
+  login: 'Login',
+};
+
+export default function LoginPage() {
+  const [loginSignUp, setLoginSignUp] = useState(inicialValue);
+
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.mainImg}>
+          <Image
+            src="/background-login.jpg"
+            alt="image-background-login"
+            fill
+            quality={100}
+          />
+        </div>
+
+        <section className={styles.mainContainer}>
+          <div className={styles['main-container-img']}>
+            <Image
+              src="/login-img.jpg"
+              alt="image-background-form-login"
+              fill
+              className={styles['img-form-login']}
+            />
+          </div>
+
+          <section className={styles.form}>
+            <header className={`${styles['form-header']}`}>
+              <span
+                onClick={() => handleLoginSignUp()}
+                className={`${
+                  loginSignUp.login == 'Login' ? styles['Login'] : ''
+                }`}
+              >
+                Login in
+              </span>
+
+              <span
+                onClick={() => handleLoginSignUp()}
+                className={`${
+                  loginSignUp.login == 'SignUp' ? styles['SignUp'] : ''
+                }`}
+              >
+                Sign in
+              </span>
+            </header>
+
+            {loginSignUp.login == 'Login' ? <FormLogin /> : <FormSignUp />}
+
+            <div className={styles.formContainerLoginApp}>
+              <div className={styles['login-apps-text']}>
+                <div className={styles['traco']}></div>
+                <span>Continue With</span>
+                <div className={styles['traco']}></div>
+              </div>
+
+              <div className={styles['login-apps']}>
+                <div className={styles['app']}>
+                  <BsFacebook size={32} className={styles['facebook']} />
                 </div>
 
-                <section className={styles.mainContainer}>
-                    <div className={styles['main-container-img']}>
-                        <Image src='/login-img.jpg' alt='image-background-form-login' fill className={styles['img-form-login']} />
-                    </div>
+                <div className={styles['app']}>
+                  <BsTwitter size={32} className={styles['twitter']} />
+                </div>
 
-                    <form className={styles.form}>
-                        <header className={styles['form-header']}>
-                            <span>Login in</span>
-                            
-                            <span>Sign in</span>
-                        </header>
-                    </form>
-                </section>
-            </main>
-        </>
-    )
+                <div className={styles['app']}>
+                  <FcGoogle size={32} className={styles['google']} />
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+      </main>
+    </>
+  );
+
+  function handleLoginSignUp() {
+    loginSignUp.login == 'Login'
+      ? setLoginSignUp({ login: 'SignUp' })
+      : setLoginSignUp({ login: 'Login' });
+  }
 }

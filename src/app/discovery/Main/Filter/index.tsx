ab3faa7136a0986eaf6a95721ca05f24d21a72af
Alerrando <alerrando2@gmail.com>
2023-03-18 17:07:15 -0300

    add style to filter menu

--- src/app/discovery/Main/Filter/index.tsx
+++ src/app/discovery/Main/Filter/index.tsx
@@ -1,18 +1,41 @@
-import styles from './styles.module.scss'
-import { GrClose } from 'react-icons/gr'
+import styles from "./styles.module.scss";
+import { GrClose } from "react-icons/gr";
+import { Disclosure, Transition } from "@headlessui/react";
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
+
 
 type FilterProps = {
-    setFilterMenu: (filterMenu: boolean) => void,
-}
+  setFilterMenu: (filterMenu: boolean) => void;
+};
 
-export default function Filter({ setFilterMenu }: FilterProps){
-    return(
-        <div className={styles.filter}>
-            <div className={styles['filter-container']}>
-                <header className={styles['filter-header']}>
-                    <GrClose size={26} onClick={() => setFilterMenu(false)} />
-                </header>
-            </div>
-        </div>
-    )
+export default function Filter({ setFilterMenu }: FilterProps) {
+  return (
+    <div className={styles.filter}>
+      <div className={styles["filter-container"]}>
+        <header className={styles["filter-header"]}>
+          <GrClose size={26} onClick={() => setFilterMenu(false)} />
+        </header>
+
+        <section className={styles['filter-section-questions-answers']}>
+          <Disclosure>
+            {({ open }) => (
+                <>
+                <Disclosure.Button className={styles["filter-question"]}>
+                  Genres
+                  <ChevronUpIcon className={`${open ? `${styles['close']}` : ''} ${styles['open']}`}  />
+                </Disclosure.Button>
+
+
+                <Disclosure.Panel className={styles["filter-answers"]}>
+                  Yes! You can purchase a license that you can share with your
+                  entire team.
+                </Disclosure.Panel>
+                
+                </>
+            )}
+            </Disclosure>
+        </section>
+      </div>
+    </div>
+  );
 }

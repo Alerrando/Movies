3506c61bd7c2c23915734cbdc8fea7560f6de667
Alerrando <alerrando2@gmail.com>
2023-04-02 16:13:35 -0300

    add genres to panel

--- src/app/(home)/Main/FeaturedFilm/index.tsx
+++ src/app/(home)/Main/FeaturedFilm/index.tsx
@@ -1,16 +1,38 @@
 import { Tab } from "@headlessui/react";
 import Image from "next/image";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { FeaturedFilmType } from "..";
 import styles from "./styles.module.scss";
-import { AiOutlineStar } from 'react-icons/ai'
+import { AiOutlineStar } from "react-icons/ai";
+import { getGenre } from "api";
 
 type FeaturedFilmProps = {
   featuredFilm: FeaturedFilmType;
 };
 
+type GenreMovies = {
+  genres: []
+}
+
+type Genre = {
+  id: number,
+  name: string,
+}
+
 export default function FeaturedFilm({ featuredFilm }: FeaturedFilmProps) {
-  console.log(featuredFilm)
+  const [genresTotal, setGenresTotal] = useState<GenreMovies>({} as GenreMovies);
+  console.log(featuredFilm);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const genre = await getGenre();
+        setGenresTotal(genre)
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  });
 
   return (
     <section className={styles.featuredFilm}>
@@ -29,9 +51,9 @@ export default function FeaturedFilm({ featuredFilm }: FeaturedFilmProps) {
           <Tab as={Fragment}>
             {({ selected }) => (
               <button
-                className={
-                  `${styles['tabble']} ${styles[selected ? "open" : "close"]}`
-                }
+                className={`${styles["tabble"]} ${
+                  styles[selected ? "open" : "close"]
+                }`}
               >
                 GENERAL INFORMATION
               </button>
@@ -40,9 +62,9 @@ export default function FeaturedFilm({ featuredFilm }: FeaturedFilmProps) {
           <Tab as={Fragment}>
             {({ selected }) => (
               <button
-                className={
-                  `${styles['tabble']} ${styles[selected ? "open" : "close"]}`
-                }
+                className={`${styles["tabble"]} ${
+                  styles[selected ? "open" : "close"]
+                }`}
               >
                 SIMILAR
               </button>
@@ -50,12 +72,26 @@ export default function FeaturedFilm({ featuredFilm }: FeaturedFilmProps) {
           </Tab>
         </Tab.List>
         <Tab.Panels className={styles.TabPanel}>
-          <Tab.Panel className={styles['panel']}>
+          <Tab.Panel className={styles["panel"]}>
             <AiOutlineStar size={18} />
+            <span>{featuredFilm.vote_average}</span>
+
+            <div className={styles["panel-genres"]}>
+              {Object.keys(genresTotal).length > 0
+                ? genresTotal.genres.map((genre: Genre, index: number) => (
+                  <>
+                      {featuredFilm.genre_ids.indexOf(genre.id) > -1 ? (
+                        <>
+                          <span>|</span>
+                          <p>{genre.name}</p>
+                        </>
+                      ) : null}
+                    </>
+                  ))
+                : null}
+            </div>
           </Tab.Panel>
-          <Tab.Panel className={styles['panel']}>
-            
-          </Tab.Panel>
+          <Tab.Panel className={styles["panel"]}></Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
     </section>
